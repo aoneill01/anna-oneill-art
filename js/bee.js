6a476ddec7b975c683220e0bbb628f7451bcb983
Andy O'Neill <aoneill01@gmail.com>
2019-09-19 20:59:55 -0400

    Clean up bee logic

--- js/bee.js
+++ js/bee.js
@@ -1,73 +1,89 @@
-const last = { x: 0, y: 0 };
-const lastKnownMousePosition = { x: 0, y: 0 };
+{
+    const bee = {
+        position: {
+            x: 0,
+            y: 0
+        },
+        direction: 0,
+        step: 0
+    };
+    let lastDash = { x: 0, y: 0 };
+    let lastDashes = [];
+    let lastKnownMousePosition = { x: 0, y: 0 };
+    let requestAnimationFrameHandler;
+    let lastUpdate = performance.now();
+    const beeDiv = document.getElementById("bee");
 
+    window.addEventListener("mousemove", e => {
+        lastKnownMousePosition = { x: e.pageX, y: e.pageY };
 
-window.addEventListener("mousemove", e => {
-    lastKnownMousePosition.x = e.pageX;
-    lastKnownMousePosition.y = e.pageY;
+        if (!requestAnimationFrameHandler) {
+            requestAnimationFrameHandler = window.requestAnimationFrame(update);
+        }
+    });
 
-    
-});
+    function updateBee() {
+        const turn = 0.03;
+        const velocity = 3;
+        bee.step++;
+        const destAngle = Math.atan2(
+            lastKnownMousePosition.y - bee.position.y,
+            lastKnownMousePosition.x - bee.position.x
+        );
 
-class Bee {
-    x = 0;
-    y = 0;
-    direction = 0;
-    velocity = 3;
-    turn = 0.03;
-    step = 0;
-
-    update(destX, destY) {
-        this.step++;
-        const destAngle = Math.atan2(destY - this.y, destX - this.x);
-
-        if (this.direction > destAngle) {
-            if (this.direction - destAngle > Math.PI) {
-                this.direction += this.turn;
+        if (bee.direction > destAngle) {
+            if (bee.direction - destAngle > Math.PI) {
+                bee.direction += turn;
+            } else {
+                bee.direction -= turn;
             }
-            else {
-                this.direction -= this.turn;
+        } else {
+            if (destAngle - bee.direction > Math.PI) {
+                bee.direction -= turn;
+            } else {
+                bee.direction += turn;
             }
         }
-        else {
-            if (destAngle - this.direction > Math.PI) {
-                this.direction -= this.turn;
-            }
-            else {
-                this.direction += this.turn;
-            }
-        }
-        if (this.direction < -Math.PI) this.direction += 2 * Math.PI;
-        if (this.direction > Math.PI) this.direction -= 2 * Math.PI;
-        const wiggleDirection = this.direction + .5 * Math.sin(.1 * this.step);
-        this.x += this.velocity * Math.cos(wiggleDirection);
-        this.y += this.velocity * Math.sin(wiggleDirection);
+        if (bee.direction < -Math.PI) bee.direction += 2 * Math.PI;
+        if (bee.direction > Math.PI) bee.direction -= 2 * Math.PI;
+        const wiggleDirection = bee.direction + 0.5 * Math.sin(0.1 * bee.step);
+        bee.position.x += velocity * Math.cos(wiggleDirection);
+        bee.position.y += velocity * Math.sin(wiggleDirection);
+        beeDiv.style.top = `${bee.position.y}px`;
+        beeDiv.style.left = `${bee.position.x}px`;
     }
-}
 
-const bee = new Bee();
-const beeDiv = document.getElementById("bee");
-let lastUpdate = performance.now();
-let request = window.requestAnimationFrame(update);
+    function updateDashes() {
+        if (
+            (lastDash.x - bee.position.x) ** 2 +
+                (lastDash.y - bee.position.y) ** 2 >
+            200
+        ) {
+            lastDash = { x: bee.position.x, y: bee.position.y };
+            lastDashes.push(lastDash);
+            if (lastDashes.length === 3) {
+                const direction = Math.atan2(
+                    lastDashes[0].y - lastDashes[2].y,
+                    lastDashes[0].x - lastDashes[2].x
+                );
+                let dash = document.createElement("div");
+                dash.classList.add("dash");
+                dash.style.top = `${lastDashes[1].y + 5}px`;
+                dash.style.left = `${lastDashes[1].x}px`;
+                dash.style.transform = `rotate(${direction}rad)`;
+                document.body.appendChild(dash);
+                setTimeout(() => dash.classList.add("fade"), 10);
+                setTimeout(() => document.body.removeChild(dash), 2100);
+                lastDashes.splice(0, 2);
+            }
+        }
+    }
 
-function update(timestamp) {
-    bee.update(lastKnownMousePosition.x, lastKnownMousePosition.y);
-    beeDiv.style.top = `${bee.y}px`;
-    beeDiv.style.left = `${bee.x}px`;
-    
-    lastUpdate = timestamp;
-    request = window.requestAnimationFrame(update);
-    
-    if ((last.x - bee.x)**2 + (last.y - bee.y)**2 < 1000) return;
-    let rotation = Math.atan2(last.y - bee.y, last.x - bee.x);
-    last.x = bee.x;
-    last.y = bee.y;
-    let dash = document.createElement("div");
-    dash.classList.add("dash");
-    dash.style.top = `${bee.y + 5}px`;
-    dash.style.left = `${bee.x}px`;
-    dash.style.transform = `rotate(${rotation}rad)`;
-    document.body.appendChild(dash);
-    setTimeout(() => dash.classList.add("fade"), 10);
-    setTimeout(() => document.body.removeChild(dash), 2100);
+    function update(timestamp) {
+        lastUpdate = timestamp;
+        updateBee();
+        updateDashes();
+
+        requestAnimationFrameHandler = window.requestAnimationFrame(update);
+    }
 }
