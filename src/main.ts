17b697639747c3bf48b8d9d3950376b0d8f7ddcd
Andy O'Neill <aoneill01@gmail.com>
2025-06-01 17:29:36 -0400

    Basic gsap setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { gsap } from "gsap";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollTrigger.create({
+  trigger: "#grass",
+  endTrigger: "#b",
+  pin: true,
+  start: "bottom bottom",
+  end: "bottom bottom",
+  scrub: true,
+});
+
+ScrollSmoother.create({
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+});
